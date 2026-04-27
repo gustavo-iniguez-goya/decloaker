29badef2b604d0c892c969568203dd5968839b76
Gustavo Iñiguez Goia <gooffy1@gmail.com>
2026-04-27 22:26:19 +0200

    cli options code reorganization

--- main.go
+++ main.go
@@ -88,96 +88,20 @@ func main() {
 		decloaker.Conntrack()
 
 	case "disk ls <paths>":
-		expected := disk.ReadDir(CLI.Disk.Dev, CLI.Disk.Partition, CLI.Disk.Ls.Paths[0], diskfs.ReadOnly, CLI.Disk.Ls.Recursive)
-		for file, stat := range expected {
-			if stat == nil {
-				continue
-			}
-			dlog.Log("%s\t%d\t%s\t%s\n",
-				stat.Mode(),
-				stat.Size(),
-				stat.ModTime().Format(time.RFC3339),
-				file)
-		}
-
-		if CLI.Disk.Ls.Compare {
-			orig, _ := decloaker.ListFiles(CLI.Disk.Ls.Paths[0], sys.CmdFind, CLI.Disk.Ls.Recursive)
-			ret = decloaker.CompareFiles(false, orig, expected)
-		}
+		ret = diskLs()
 	case "disk cp <orig> <dest>":
-		err := disk.Cp(CLI.Disk.Dev, CLI.Disk.Partition, CLI.Disk.Cp.Orig, CLI.Disk.Cp.Dest, diskfs.ReadOnly)
-		if err != nil {
-			dlog.Error("%s\n", err)
-			ret = decloaker.ERROR
-		} else {
-			dlog.Ok("OK\n")
-		}
+		diskCp()
 		// not implemented in go-diskfs
 	case "disk mv <orig> <dest>":
-		err := disk.Mv(CLI.Disk.Dev, CLI.Disk.Partition, CLI.Disk.Cp.Orig, CLI.Disk.Cp.Dest, diskfs.ReadOnly)
-		if err != nil {
-			dlog.Error("%s\n", err)
-			ret = decloaker.ERROR
-		} else {
-			dlog.Ok("OK\n")
-		}
+		ret = diskMv()
 	case "disk stat <paths>":
-		list, err := disk.Stat(CLI.Disk.Dev, CLI.Disk.Partition, CLI.Disk.Stat.Paths, diskfs.ReadOnly)
-		if err != nil {
-			ret = decloaker.ERROR
-			dlog.Error("%s\n", err)
-		} else {
-			for _, file := range list {
-				dlog.Detection("%s\t%d\t%s\t%s\n", file.Mode(), file.Size(), file.ModTime().Format(time.RFC3339), file.Name())
-				utils.PrintFileExtendedInfo(file.Sys())
-			}
-		}
-
+		ret = diskStat()
 	case "disk cat <path>":
-		content, err := disk.ReadFile(CLI.Disk.Dev, CLI.Disk.Partition, CLI.Disk.Cat.Path)
-		if err != nil {
-			dlog.Error("%s\n", err)
-			ret = decloaker.ERROR
-		} else {
-			dlog.Ok("cat %s:\n\n", CLI.Disk.Cat.Path)
-			dlog.Detection("%s", content)
-			dlog.Log("\n")
-
-			if CLI.Disk.Cat.Compare {
-				orig := sys.Cat("cat", CLI.Disk.Cat.Path)
-				origSize := len(orig)
-				ret = decloaker.CompareContent(CLI.Disk.Cat.Path, orig[CLI.Disk.Cat.Path], string(content), origSize, len(content), "raw")
-			}
-		}
-
+		ret = diskCat()
 	case "disk rm <paths>":
-		err := disk.Rm(CLI.Disk.Dev, CLI.Disk.Partition, CLI.Disk.Rm.Paths, diskfs.ReadWrite)
-		if err != nil {
-			dlog.Error("%s\n", err)
-			ret = decloaker.ERROR
-		} else {
-			dlog.Ok("rm %v\n\n", CLI.Disk.Rm.Paths)
-		}
+		ret = diskRm()
 	case "disk find <paths>":
-		files := disk.Find(
-			CLI.Disk.Dev,
-			CLI.Disk.Partition,
-			CLI.Disk.Find.Paths[0],
-			CLI.Disk.Find.Inode,
-			CLI.Disk.Find.Name,
-			diskfs.ReadOnly,
-			CLI.Disk.Find.Recursive,
-		)
-		for file, stat := range files {
-			if stat == nil {
-				continue
-			}
-			dlog.Log("%s\t%d\t%s\t%s\n",
-				stat.Mode(),
-				stat.Size(),
-				stat.ModTime().Format(time.RFC3339),
-				file)
-		}
+		diskFind()
 
 	case "scan hidden-files":
 		ret = scanHiddenFiles()
@@ -193,6 +117,8 @@ func main() {
 		ret = decloaker.CheckHiddenProcs(CLI.Scan.HiddenProcs.BruteForce, CLI.Scan.HiddenProcs.MaxPid)
 	case "scan suspicious-procs":
 		ret = scanSuspiciousProcs()
+	case "scan procs":
+		ret = scanProcs()
 	case "scan hidden-sockets <protos>":
 		ret = decloaker.CheckHiddenSockets(CLI.Scan.HiddenSockets.Protos)
 	case "scan hidden-sockets":
@@ -206,44 +132,11 @@ func main() {
 		ret = decloaker.CheckHiddenProcs(CLI.Scan.HiddenProcs.BruteForce, 0)
 
 	case "dump files":
-		dlog.Detection("%-10s %-10s %-6s %-8s %-5s %-5s %s %-16s %s\t%s\n",
-			"Pid", "PPid", "Fd", "Inode", "UID", "GID", "Host", "Comm", "File", "Exe")
-		files := ebpf.GetFileList(CLI.Dump.Files.Host)
-		for _, f := range files {
-			dlog.Detection("%-10s %-10s %-6s %-8s %-5s %-5s %s %-16s %s\t%s\n",
-				f.Pid, f.PPid,
-				f.Fd, f.Inode,
-				f.Uid, f.Gid,
-				f.Hostname,
-				f.Comm, f.File, f.Exe,
-			)
-		}
+		dumpFiles()
 	case "dump kmods":
-		dlog.Detection("%-20s\t%-10s\t%s\t%s\t%s\n",
-			"Name", "Type", "Symbol", "Address", "Function")
-		kmods := ebpf.GetKmodList()
-		for _, k := range kmods {
-			dlog.Detection("%-20s\t%-10s\t%s\t%s\t%s\n",
-				k.Name,
-				k.Type,
-				k.AType,
-				k.Addr,
-				k.Func,
-			)
-		}
+		dumpKmods()
 	case "dump tasks":
-		dlog.Detection("%-10s %-10s %-8s %-8s %-8s %-16s %-16s %s\n",
-			"Pid", "PPid", "Inode", "UID", "GID", "Host", "Comm", "Exe")
-		tasks := ebpf.GetPidList(CLI.Dump.Tasks.Host)
-		for _, t := range tasks {
-			dlog.Detection("%-10s %-10s %-8s %-8s %-8s %-16s %-16s %s\n",
-				t.Pid, t.PPid,
-				t.Inode,
-				t.Uid, t.Gid,
-				t.Hostname,
-				t.Comm, t.Exe,
-			)
-		}
+		dumpTasks()
 
 	/* TODO
 	case "config set":
@@ -399,16 +292,201 @@ func printLs(showExtendedInfo bool) {
 	}
 }
 
-/*func checkAll(paths []string, deep bool) {
+func diskLs() int {
+	ret := decloaker.OK
+
+	expected := disk.ReadDir(CLI.Disk.Dev, CLI.Disk.Partition, CLI.Disk.Ls.Paths[0], diskfs.ReadOnly, CLI.Disk.Ls.Recursive)
+	for file, stat := range expected {
+		if stat == nil {
+			continue
+		}
+		dlog.Log("%s\t%d\t%s\t%s\n",
+			stat.Mode(),
+			stat.Size(),
+			stat.ModTime().Format(time.RFC3339),
+			file)
+	}
+
+	if CLI.Disk.Ls.Compare {
+		orig, _ := decloaker.ListFiles(CLI.Disk.Ls.Paths[0], sys.CmdFind, CLI.Disk.Ls.Recursive)
+		ret = decloaker.CompareFiles(false, orig, expected)
+	}
+
+	return ret
 }
 
-func runConfigSet(key, value string) {
-	fmt.Printf("Setting config: %s = %s\n", key, value)
-	// TODO: Save config to file or DB
+func diskFind() {
+	files := disk.Find(
+		CLI.Disk.Dev,
+		CLI.Disk.Partition,
+		CLI.Disk.Find.Paths[0],
+		CLI.Disk.Find.Inode,
+		CLI.Disk.Find.Name,
+		diskfs.ReadOnly,
+		CLI.Disk.Find.Recursive,
+	)
+	for file, stat := range files {
+		if stat == nil {
+			continue
+		}
+		dlog.Log("%s\t%d\t%s\t%s\n",
+			stat.Mode(),
+			stat.Size(),
+			stat.ModTime().Format(time.RFC3339),
+			file)
+	}
 }
 
-func runConfigGet(key string) {
-	fmt.Printf("Retrieving config for key: %s\n", key)
-	// TODO: Load config from file or DB
-	fmt.Println("Value: <mocked-value>")
-}*/
+func diskMv() int {
+	ret := decloaker.OK
+
+	err := disk.Mv(CLI.Disk.Dev, CLI.Disk.Partition, CLI.Disk.Cp.Orig, CLI.Disk.Cp.Dest, diskfs.ReadOnly)
+	if err != nil {
+		dlog.Error("%s\n", err)
+		ret = decloaker.ERROR
+	} else {
+		dlog.Ok("OK\n")
+	}
+
+	return ret
+}
+
+func diskCat() int {
+	ret := decloaker.OK
+	content, err := disk.ReadFile(CLI.Disk.Dev, CLI.Disk.Partition, CLI.Disk.Cat.Path)
+	if err != nil {
+		dlog.Error("%s\n", err)
+		ret = decloaker.ERROR
+	} else {
+		dlog.Ok("cat %s:\n\n", CLI.Disk.Cat.Path)
+		dlog.Detection("%s", content)
+		dlog.Log("\n")
+
+		if CLI.Disk.Cat.Compare {
+			orig := sys.Cat("cat", CLI.Disk.Cat.Path)
+			origSize := len(orig)
+			ret = decloaker.CompareContent(CLI.Disk.Cat.Path, orig[CLI.Disk.Cat.Path], string(content), origSize, len(content), "raw")
+		}
+	}
+
+	return ret
+}
+
+func diskRm() int {
+	ret := decloaker.OK
+	err := disk.Rm(CLI.Disk.Dev, CLI.Disk.Partition, CLI.Disk.Rm.Paths, diskfs.ReadWrite)
+	if err != nil {
+		dlog.Error("%s\n", err)
+		ret = decloaker.ERROR
+	} else {
+		dlog.Ok("rm %v\n\n", CLI.Disk.Rm.Paths)
+	}
+
+	return ret
+}
+
+func diskCp() int {
+	ret := decloaker.OK
+	err := disk.Cp(CLI.Disk.Dev, CLI.Disk.Partition, CLI.Disk.Cp.Orig, CLI.Disk.Cp.Dest, diskfs.ReadOnly)
+	if err != nil {
+		dlog.Error("%s\n", err)
+		ret = decloaker.ERROR
+	} else {
+		dlog.Ok("OK\n")
+	}
+
+	return ret
+}
+
+func diskStat() int {
+	ret := decloaker.OK
+	list, err := disk.Stat(CLI.Disk.Dev, CLI.Disk.Partition, CLI.Disk.Stat.Paths, diskfs.ReadOnly)
+	if err != nil {
+		ret = decloaker.ERROR
+		dlog.Error("%s\n", err)
+	} else {
+		for _, file := range list {
+			dlog.Detection("%s\t%d\t%s\t%s\n", file.Mode(), file.Size(), file.ModTime().Format(time.RFC3339), file.Name())
+			utils.PrintFileExtendedInfo(file.Sys())
+		}
+	}
+
+	return ret
+}
+
+func dumpFiles() {
+	dlog.Detection("%-10s %-10s %-6s %-8s %-5s %-5s %s %-16s %s\t%s\n",
+		"Pid", "PPid", "Fd", "Inode", "UID", "GID", "Host", "Comm", "File", "Exe")
+	files := ebpf.GetFileList(CLI.Dump.Files.Host)
+	for _, f := range files {
+		dlog.Detection("%-10s %-10s %-6s %-8s %-5s %-5s %s %-16s %s\t%s\n",
+			f.Pid, f.PPid,
+			f.Fd, f.Inode,
+			f.Uid, f.Gid,
+			f.Hostname,
+			f.Comm, f.File, f.Exe,
+		)
+		dlog.Event("dump_file", "open file descriptor",
+			dlog.F{
+				"pid":      f.Pid,
+				"ppid":     f.PPid,
+				"fd":       f.Fd,
+				"inode":    f.Inode,
+				"uid":      f.Uid,
+				"gid":      f.Gid,
+				"hostname": f.Hostname,
+				"comm":     f.Comm,
+				"file":     f.File,
+				"exe":      f.Exe,
+			})
+	}
+}
+
+func dumpKmods() {
+	dlog.Detection("%-20s\t%-10s\t%s\t%s\t%s\n",
+		"Name", "Type", "Symbol", "Address", "Function")
+	kmods := ebpf.GetKmodList()
+	for _, k := range kmods {
+		dlog.Detection("%-20s\t%-10s\t%s\t%s\t%s\n",
+			k.Name,
+			k.Type,
+			k.AType,
+			k.Addr,
+			k.Func,
+		)
+		dlog.Event("dump_kmod", "loaded kernel module",
+			dlog.F{
+				"name":   k.Name,
+				"type":   k.Type,
+				"symbol": k.AType,
+				"addr":   k.Addr,
+				"func":   k.Func,
+			})
+	}
+}
+
+func dumpTasks() {
+	dlog.Detection("%-10s %-10s %-8s %-8s %-8s %-16s %-16s %s\n",
+		"Pid", "PPid", "Inode", "UID", "GID", "Host", "Comm", "Exe")
+	tasks := ebpf.GetPidList(CLI.Dump.Tasks.Host)
+	for _, t := range tasks {
+		dlog.Detection("%-10s %-10s %-8s %-8s %-8s %-16s %-16s %s\n",
+			t.Pid, t.PPid,
+			t.Inode,
+			t.Uid, t.Gid,
+			t.Hostname,
+			t.Comm, t.Exe,
+		)
+		dlog.Event("dump_task", "running task",
+			dlog.F{
+				"pid":      t.Pid,
+				"ppid":     t.PPid,
+				"inode":    t.Inode,
+				"uid":      t.Uid,
+				"gid":      t.Gid,
+				"hostname": t.Hostname,
+				"comm":     t.Comm,
+				"exe":      t.Exe,
+			})
+	}
+}
